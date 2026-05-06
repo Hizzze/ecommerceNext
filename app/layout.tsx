55e6dc1b34c5c1b02d2fbb853e5fe3ba410229bb
Vlad Syzov <hizes5050@gmail.com>
2026-05-06 22:09:39 +0200

    carousel and home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Navbar } from '@/components/navbar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,9 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">
+      <body className="flex min-h-full flex-col bg-white">
         <Navbar />
-        <main>{children}</main>
+        <main className="flex-grow container mx-auto px-4 py-8">{children}</main>
       </body>
     </html>
   );
